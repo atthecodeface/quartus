1e895b005c580e81c6c525fc077f9f95eb89ece5
Gavin J Stark <atthecodeface@gmail.com>
2019-10-17 10:42:37 +0100

    [vivado memories] Updated create_mmi to have command line arguments

--- vivado/scripts/route.tcl
+++ vivado/scripts/route.tcl
@@ -7,7 +7,7 @@ open_checkpoint ${output_file_base}.postphysopt.dcp
 
 route_design
 write_checkpoint -force ${output_file_base}.postroute.dcp
-set bram_dict_py [open ${output_file_base}.postroute.bram_dict.py w]
-report_cells_as_pydict {$bram_dict_py {REF_NAME =~ *RAMB*}}
+set bram_dict_py [open ${output_file_base}__bram_dict.py w]
+report_cells_as_pydict $bram_dict_py {REF_NAME =~ *RAMB*}
 close $bram_dict_py
 
