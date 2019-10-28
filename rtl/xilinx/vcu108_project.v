2a4aaf20b3b2d48c9a2193bac1781cb4d5a9b891
Gavin Stark <atthecodeface@gmail.com>
2019-10-28 09:40:37 +0000

    [vcu108] Improving clock measurement

--- rtl/xilinx/vcu108_project.v
+++ rtl/xilinx/vcu108_project.v
@@ -42,7 +42,14 @@ module vcu108_project ( input SYS_CLK1__p, input SYS_CLK1__n,
    wire         clk_locked;
    wire         reset_n;
 
-   wire [63:0] vcu108_data;
+   wire         vcu108_dprintf_req__valid;
+   wire [15:0]  vcu108_dprintf_req__address;
+   wire [63:0]  vcu108_dprintf_req__data_0;
+   wire [63:0]  vcu108_dprintf_req__data_1;
+   wire [63:0]  vcu108_dprintf_req__data_2;
+   wire [63:0]  vcu108_dprintf_req__data_3;
+   wire         vcu108_dprintf_ack;
+
 
    wire         sysclk1;
    IBUFDS sys_clk1_buf( .I(SYS_CLK1__p), .IB(SYS_CLK1__n), .O(sysclk1) );
@@ -57,23 +64,24 @@ module vcu108_project ( input SYS_CLK1__p, input SYS_CLK1__n,
 
    assign video_clk = clk_150;
    assign flash_clk = clk_50;
+   wire reset_in = vcu108_inputs__buttons[1]
    assign reset_n = clk_locked;
 
-   pll_base video_clk_gen( .refclk(sysclk1), .rst(vcu108_inputs__buttons[1]),
+   pll_base video_clk_gen( .refclk(sysclk1), .rst(reset_in),
                            .outclk_225(clk_225),
                            .outclk_150(clk_150),
                            .outclk_100(clk_100),
                            .outclk_50(clk_50),
                            .locked(clk_locked) );
 
-   pll_sgmii_tx sgmii_pll_tx ( .refclk(clk125mhz), .rst(vcu108_inputs__buttons[1]),
+   pll_sgmii_tx sgmii_pll_tx ( .refclk(clk125mhz), .rst(reset_in),
                          .outclk_625(tx_clk_625),
                          .outclk_312_5(tx_clk_312_5),
                          .outclk_125(tx_clk_125),
                          .outclk_25(tx_clk_25),
                          .locked(sgmii_tx_clk_locked) );
    
-   pll_sgmii_rx sgmii_pll_rx ( .refclk(sgmii_rxclk), .rst(vcu108_inputs__buttons[1]),
+   pll_sgmii_rx sgmii_pll_rx ( .refclk(sgmii_rxclk), .rst(reset_in),
                          .outclk_625(rx_clk_625),
                          .outclk_312_5(rx_clk_312_5),
                          .outclk_125(rx_clk_125),
@@ -94,7 +102,14 @@ module vcu108_project ( input SYS_CLK1__p, input SYS_CLK1__n,
                       .flash_clk(flash_clk),
                       .flash_clk__enable(1),
 
-                      .vcu108_data(vcu108_data),
+                      .vcu108_dprintf_req__valid(vcu108_dprintf_req__valid),
+                      .vcu108_dprintf_req__address(vcu108_dprintf_req__address),
+                      .vcu108_dprintf_req__data_0(vcu108_dprintf_req__data_0),
+                      .vcu108_dprintf_req__data_1(vcu108_dprintf_req__data_1),
+                      .vcu108_dprintf_req__data_2(vcu108_dprintf_req__data_2),
+                      .vcu108_dprintf_req__data_3(vcu108_dprintf_req__data_3),
+                      .vcu108_dprintf_ack(vcu108_dprintf_ack),
+
                       .vcu108_inputs__switches(vcu108_inputs__switches),
                       .vcu108_inputs__buttons(vcu108_inputs__buttons),
                       .vcu108_inputs__uart_rx__rxd(uart_rxd),
@@ -130,38 +145,73 @@ module vcu108_project ( input SYS_CLK1__p, input SYS_CLK1__n,
    
    assign hdmi__clk = video_clk;
 
-wire cpm_clk;
-assign cpm_clk = rx_clk_125;
-wire [8:0]delay_config__value;
-cascaded_delay_pair cdp(.clk(cpm_clk),
-                            .reset(!reset_n),
-                            .delay__load(delay_config__load),
-                            .delay__value(delay_config__value),
-                            .data_in(sgmii_rxclk),
-                            .data_out(cdp_delay_out)
-);
-(*  ASYNC_REG = "TRUE",  shreg_extract = "no"   *)
-FDPE s_0 (.PRE (0),.C (cpm_clk), .CE (1), .D (cdp_delay_out), .Q (d0) );
-FDPE s_1 (.PRE (0),.C (cpm_clk), .CE (1), .D (d0), .Q (d1) );
-FDPE s_2 (.PRE (0),.C (cpm_clk), .CE (1), .D (d1), .Q (d2) );
-    clocking_phase_measure cpm( .clk(cpm_clk),
-    .clk__enable(1),
-    .reset_n(reset_n),
+   wire measure_response__valid;
+   wire measure_response__initial_value;
+   wire measure_response__abort;
+   wire [8:0] measure_response__delay;
+   wire [8:0] measure_response__initial_delay;
+   wire       cpm_clk;
+   assign cpm_clk = rx_clk_125;
+   wire [8:0] delay_config__value;
+   cascaded_delay_pair cdp(.clk(cpm_clk),
+                           .reset(!reset_n),
+                           .delay__load(delay_config__load),
+                           .delay__value(delay_config__value),
+                           .data_in(sgmii_rxclk),
+                           .data_out(cdp_delay_out)
+                           );
+   (*  ASYNC_REG = "TRUE",  shreg_extract = "no"   *)
+   FDPE s_0 (.PRE (0),.C (cpm_clk), .CE (1), .D (cdp_delay_out), .Q (d0) );
+   (*  ASYNC_REG = "TRUE",  shreg_extract = "no"   *)
+   FDPE s_1 (.PRE (0),.C (cpm_clk), .CE (1), .D (d0), .Q (d1) );
+   (*  ASYNC_REG = "TRUE",  shreg_extract = "no"   *)
+   FDPE s_2 (.PRE (0),.C (cpm_clk), .CE (1), .D (d1), .Q (d2) );
+   clocking_phase_measure cpm( .clk(cpm_clk),
+                               .clk__enable(1),
+                               .reset_n(reset_n),
 
-    .delay_response__load_ack(1),
-    .delay_response__value(0),
-    .delay_response__sync_value(d2),
-    .delay_config__load(delay_config__load),
-    .delay_config__value(delay_config__value),
+                               .delay_response__load_ack(1),
+                               .delay_response__value(0),
+                               .delay_response__sync_value(d2),
+                               .delay_config__load(delay_config__load),
+                               .delay_config__value(delay_config__value),
 
-    .measure_request__valid(1),
-    //.measure_response__ack(1),
-    .measure_response__valid(vcu108_data[0]),
-    .measure_response__initial_value(vcu108_data[1]),
-    .measure_response__abort(vcu108_data[2]),
-    .measure_response__delay(vcu108_data[24:16]),
-    .measure_response__initial_delay(vcu108_data[40:32])
-);
+                               .measure_request__valid(1),
+                               //.measure_response__ack(1),
+                               .measure_response__valid(measure_response__valid),
+                               .measure_response__initial_value(measure_response__initial_value),
+                               .measure_response__abort(measure_response__abort),
+                               .measure_response__delay(measure_response__delay),
+                               .measure_response__initial_delay(measure_response__initial_delay),
+                               );
 
+   dprintf_4_async d4a( .clk_in(cpm_clk),
+                        .clk_in__enable(1),
+                        .clk_out( `dut_clk),
+                        .clk_out__enable(1),
+                        .reset_n(reset_n),
+                        .req_in__valid(measure_response__valid),
+                        .req_in__address(80),
+                        .req_in__data_0({32h87,
+                                         7h0,
+                                         measure_response__initial_delay,
+                                         3h0,
+                                         measure_response__delay,
+                                         3h0,
+                                         measure_response__initial_value,
+                                         measure_response__abort,
+                                         measure_response__valid}),
+                        .req_in__data_1({8hff,56h0}),
+                        .req_in__data_2(0),
+                        .req_in__data_3(0),
+                        // ack_in, Ack back to clk_in domain
+                        .req_out__valid(vcu108_dprintf_req__valid),
+                        .req_out__address(vcu108_dprintf_req__address),
+                        .req_out__data_0(vcu108_dprintf_req__data_0),
+                        .req_out__data_1(vcu108_dprintf_req__data_1),
+                        .req_out__data_2(vcu108_dprintf_req__data_2),
+                        .req_out__data_3(vcu108_dprintf_req__data_3),
+                        ack_out(vcu108_dprintf_ack)
+                        );
 
 endmodule
