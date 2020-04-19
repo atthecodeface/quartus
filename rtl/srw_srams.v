4bdf07d265dd0151afda2f19545ff8dc09151b9a
Gavin Stark <gjstark@me.com>
2020-04-19 14:53:11 +0100

    [srams] Changed 16kx8 to include write_enable - all RAMs have a write_enable

--- rtl/srw_srams.v
+++ rtl/srw_srams.v
@@ -58,13 +58,14 @@ module se_sram_srw_16384x40( sram_clock, sram_clock__enable, write_data, address
     se_sram_srw #(address_width,data_width,initfile) ram(sram_clock,sram_clock__enable,write_data,address,read_not_write,select,data_out);
 endmodule
 //m se_sram_srw_16384x8
-module se_sram_srw_16384x8( sram_clock, sram_clock__enable, write_data, address, read_not_write, select, data_out );
+module se_sram_srw_16384x8( sram_clock, sram_clock__enable, write_data, address, write_enable, read_not_write, select, data_out );
     parameter initfile="",address_width=14,data_width=8;
     input sram_clock, sram_clock__enable, select, read_not_write;
+    input write_enable;
     input [address_width-1:0] address;
     input [data_width-1:0]    write_data;
     output [data_width-1:0]   data_out;
-    se_sram_srw #(address_width,data_width,initfile) ram(sram_clock,sram_clock__enable,write_data,address,read_not_write,select,data_out);
+    se_sram_srw_we #(address_width,data_width,initfile) ram(sram_clock,sram_clock__enable,write_data,address,write_enable,read_not_write,select,data_out);
 endmodule
 //m se_sram_srw_16384x32_we8
 module se_sram_srw_16384x32_we8( sram_clock, sram_clock__enable, write_data, address, write_enable, read_not_write, select, data_out );
